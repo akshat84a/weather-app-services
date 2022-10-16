33c62dcd493bd974d3f1b2d65e6f8c67d2cdbce4
Akshat Chaurasia <akshat.chaurasia1511@gmail.com>
2022-10-16 16:42:30 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+console.log('Hello, Nice to meet you.')
+
+
+const wForm=document.querySelector('form')
+const sElement=document.querySelector('input')
+const message1=document.querySelector('#m1')
+const message2=document.querySelector('#m2')
+
+
+wForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location=sElement.value
+    message1.textContent='Loading...'
+    message2.textContent=''
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error)
+        message1.textContent=data.error
+        else{
+            message2.textContent=data.forecast
+            message1.textContent=data.location
+        }
+    })
+
+})
+})
