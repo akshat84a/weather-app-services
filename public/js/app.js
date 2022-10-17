f8175109019f6193f105ac625e691a7b3bc518a6
Akshat Chaurasia <akshat.chaurasia1511@gmail.com>
2022-10-17 11:32:27 +0530

    Setup of app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ wForm.addEventListener('submit',(e)=>{
     const location=sElement.value
     message1.textContent='Loading...'
     message2.textContent=''
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error)
         message1.textContent=data.error
